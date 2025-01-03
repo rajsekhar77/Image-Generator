e8cbebebbae69676d039c94c5a5b0badeb0ffae0
rajsekhar77 <rajasekhar.b5401@gmail.com>
2025-01-03 15:48:40 +0530

    basic routes setup is completed for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
 
+function App() {
   return (
-    <>
-      <div className="bg-black text-white">hello world</div>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
