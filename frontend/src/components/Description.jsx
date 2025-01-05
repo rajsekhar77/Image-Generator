d6b03248c990df05517dec7031477b617385ccf8
rajsekhar77 <rajasekhar.b5401@gmail.com>
2025-01-05 12:07:24 +0530

    resutl page ui is completed

--- frontend/src/components/Description.jsx
+++ frontend/src/components/Description.jsx
@@ -7,7 +7,11 @@ const Description = () => {
       <h1 className="text-3xl sm:text-4xl font-semibold mb-2 text-center">
         Create AI Images
       </h1>
-      <p className="text-gray-500 mb-8 text-center">Turn your imaginations into visuals</p>
+
+      <p className="text-gray-500 mb-8 text-center">
+        Turn your imaginations into visuals
+      </p>
+
       <div className="flex flex-col gap-5 md:gap-14 md:flex-row">
         <img
           src={assets.sample_img_1}
@@ -15,7 +19,9 @@ const Description = () => {
           className="w-80 xl:w-96 rounded-lg"
         />
         <div>
-          <h2 className="text-3xl font-medium max-w-lg mb-4">Introducing the AI-Powered Text to Image Generator</h2>
+          <h2 className="text-3xl font-medium max-w-lg mb-4">
+            Introducing the AI-Powered Text to Image Generator
+          </h2>
           <p className="text-gray-600 mb-4">
             Easily bring your ideas to life with our free AI image generator.
             Whether you need stunning visuals or unique imagery, our tool
